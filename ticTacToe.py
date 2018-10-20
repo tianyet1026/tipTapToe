00396b65fb63f972bc3a64bfbfc8f707ea02152c
Alex Liu <tianyet1026@gmail.com>
2018-10-20 09:59:47 +0800

    Add code

---
+++ ticTacToe.py
@@ -0,0 +1,23 @@
+theBoard={'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+		  'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+		  'low-L': ' ', 'low-M': ' ', 'low-R': ' ',}
+
+def printBoard(board):
+	print(board['top-L']+'|'+board['top-M']+'|'+board['top-R'])
+	print('-+-+-')
+	print(board['mid-L']+'|'+board['mid-M']+'|'+board['mid-R'])
+	print('-+-+-')
+	print(board['low-L']+'|'+board['low-M']+'|'+board['low-R'])
+
+turn = 'X'
+for i in range(9):
+	printBoard(theBoard)
+	print('請 '+ turn + ' 方決定要下在那一格?')
+	move = input()
+	theBoard[move] = turn
+	if turn == 'X':
+		turn = 'O'
+	else:
+		turn = 'X'
+
+printBoard(theBoard)
